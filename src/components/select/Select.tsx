3db33d8be20ea10b837ffb275c43eb487226f1dd
oagree0123 <oagree@nvaer.com>
2024-10-30 22:29:39 +0900

    feat: Select Trigger with button element

--- src/components/select/Select.tsx
+++ src/components/select/Select.tsx
@@ -1,10 +1,24 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
+import {
+  createContext,
+  MouseEventHandler,
+  ReactNode,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+
+interface PropsWithChildren {
+  children?: ReactNode;
+  className?: string;
+}
 
 type SelectContextValue = {
   value?: string;
-  setValue?(value: string): void;
+  setValue?: (value: string) => void;
   isOpen?: boolean;
-  setIsOpen?(value: boolean): void;
+  setIsOpen?: (value: boolean) => void;
+  toggle: () => void;
 };
 
 const SelectContext = createContext<SelectContextValue | undefined>(undefined);
@@ -17,25 +31,79 @@ export const useSelectContext = (): SelectContextValue => {
   return context;
 };
 
-interface SelectProps {
-  children?: ReactNode;
+interface SelectProps extends PropsWithChildren {
   defaultValue?: string;
   defaultOpen?: boolean;
 }
 
-export const Select = ({
+const Select = ({
   children,
+  className,
   defaultValue,
   defaultOpen,
+  ...props
 }: SelectProps) => {
+  const localRef = useRef<HTMLDivElement>(null);
+
   const [value, setValue] = useState(defaultValue);
   const [isOpen, setIsOpen] = useState(defaultOpen);
 
+  const toggle = () => setIsOpen((prev) => !prev);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        localRef.current &&
+        !localRef.current.contains(event.target as Node)
+      ) {
+        close();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
-    <SelectContext.Provider value={{ value, setValue, isOpen, setIsOpen }}>
-      {children}
+    <SelectContext.Provider
+      value={{ value, setValue, isOpen, setIsOpen, toggle }}
+    >
+      <div
+        className={className}
+        ref={localRef}
+        {...props}
+      >
+        {children}
+      </div>
     </SelectContext.Provider>
   );
 };
+
+interface TriggerProps extends PropsWithChildren {
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+const Trigger = ({ children, className, onClick, ...props }: TriggerProps) => {
+  const { toggle } = useSelectContext();
+
+  const handleTriggerClick: MouseEventHandler<HTMLButtonElement> = (e) => {
+    toggle();
+    if (onClick) onClick(e);
+  };
+
+  return (
+    <button
+      className={className}
+      onClick={handleTriggerClick}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+Select.Trigger = Trigger;
 
 export default Select;
